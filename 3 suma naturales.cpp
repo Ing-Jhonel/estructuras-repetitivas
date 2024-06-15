6ce11016c6879943016f419d3a0052683a56b691
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-15 10:12:31 -0500

    2 modificacion: condicion no negativos, no ceros || Dia 2: agregue ejercicio 3 suma naturales

--- 3 suma naturales.cpp
+++ 3 suma naturales.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 using namespace std;
 // Realizar un algoritmo que sume los N primeros números naturales usando bucles.
-// ENTRADA: pnn: primeros numeros naturales
-// SALIDA: s: suma de los pnn
+// ENTRADA: primerosnumerosnaturales: primeros numeros naturales
+// SALIDA: suma: suma de los pnn
 
 int main (){
 	
-	int pnn, s;
+	int primerosnumerosnaturales, suma;
 	cout << "Indique cuantos primeros numeros naturales quiere sumar: "; 
-	cin >> pnn;
+	cin >> primerosnumerosnaturales;
 	
-	int i = 0;
-	while (i < pnn){
-		i = i + 1;
-		s =  i*(i + 1)/2;
+	while(primerosnumerosnaturales<=0){
+		cout << endl;
+		if(primerosnumerosnaturales==0){
+			cout << "No ceros. Intente de nuevo." << endl;
+		} else if(primerosnumerosnaturales<0){
+			cout << "No negativos. Intente de nuevo." << endl;
+		}
+		cout << "Indique cuantos primeros numeros naturales quiere sumar: ";
+		cin >> primerosnumerosnaturales;	
+	}
+	for (int i=1; i<=primerosnumerosnaturales; i++){
+		suma = suma + i;
 	}
 	cout << endl;
-	cout << "La suma de los " << i << " primeros numeros naturales es: " << s << endl;
+	cout << "La suma de los " << primerosnumerosnaturales << " primeros numeros naturales es: " << suma << endl;
 	return 0;
 }
