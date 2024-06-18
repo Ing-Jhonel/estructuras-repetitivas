f074fcd94d5fdc5f62db3f16b5d0b88b67439dde
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-18 11:08:03 -0500

    5.1 modificacion: mejore codificacion || Dia 5: agregue ejercicio 9 digitos

--- 9 digitos.cpp
+++ 9 digitos.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
 using namespace std;
 // Determine la cantidad de dígitos que tiene un número positivo
-// ENTRADA: n: numero ingresado
-// SALIDA: cd: cantidad de digitos
+// ENTRADA: numero: numero ingresado
+// SALIDA: cantidaddedigitos: cantidad de digitos
 
 int main (){
 	
-	int n;
+	long long int numero;
 	cout << "Introduzca un numero positivo: " << endl << endl;
-	cin >> n;
+	cin >> numero;
 	cout << endl;
 // El return 1 sirve para que el programa termine en ese punto y no continue. (se puede usar el while dentro del if, igual, las 2 son validas)
-	do{
-		if (n<0){
-			cout << "El numero es negativo. Intente de nuevo." << endl;
-		}
-		
-	} while(n<0);
+	while (numero<0){
+		cout << "El numero es negativo. Intente de nuevo." << endl;
+		cin >> numero;
+	}
 
-	
-
-	int cd = 0;
-	int aux = n;
+	int cantidaddedigitos = 0;
 
 // se usa int para que al ser un decimal, el programa lo entiende como 0, ya que en el int, un numero debe ser entero, no decimal, y cuando llega
 // a ese punto, es porque ya no hay que dividir nada mas, y se agrega 1 al contador de digitos.
-
-	while (n > 0){
-		aux = aux/10;
-		cd = cd + 1;
-	}
-	
-	if(n==0){
-		cd=1;
-	}
-	
-	
-	cout << "Digitos del numero: " << cd << endl;
+	if(numero==0){
+		cantidaddedigitos=1;
+	} else {
+		while (numero!=0){
+			numero = numero/10;
+			cantidaddedigitos = cantidaddedigitos + 1;
+		}
+	}	
+	cout << "Digitos del numero: " << cantidaddedigitos << endl;
 	return 0;
 }
