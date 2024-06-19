b92c7aaaa3f615290b8106bcb556fc0c7c49d7a7
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-19 10:44:13 -0500

    6.1 modificacion: especifique variables || Dia 6: agregue ejercicio 11 factorial

--- 11 factorial.cpp
+++ 11 factorial.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 //. Diseña un algoritmo que reciba un número entero positivo y calcule su factorial. El factorial de un número, representado como n!, 
 // es el producto de todos los números positivos menores o iguales a n. Por ejemplo: 5! = 5 × 4 × 3 × 2 × 1 = 120.
-// ENTRADA: n: numero ingresado    
-// SALIDA: fac: factorial del numero
+// ENTRADA: numero: numero ingresado    
+// SALIDA: factorial: factorial del numero
 
 int main() {
-    int n, fac=1;
+    int numero, factorial=1;
     do{
-        cout << "Ingrese el n: ";
-        cin >> n;
-        if(n < 0){
+        cout << "Ingrese el numero: ";
+        cin >> numero;
+        if(numero < 0){
             cout << "el numero debe ser un entero positivo" << endl;
         }
-    }while(n < 0);
+    }while(numero < 0);
 
-    for (int i=1; i<=n; ++i) {
-        fac = fac*i;
+    for (int i=1; i<=numero; ++i) {
+        factorial = factorial*i;
     }
 
-    cout << "El factorial de " << n << " es: " << fac << endl;
+    cout << "El factorial de " << numero << " es: " << factorial << endl;
 
     return 0;
 }
