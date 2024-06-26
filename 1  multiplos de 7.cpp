28328b761392ee77fb4d9fbfd76e6d7fe01b9bce
Alexander <achicallag@unjbg.edu.pe>
2024-06-25 22:56:03 -0500

    Cambio de estructura

--- 1  multiplos de 7.cpp
+++ 1  multiplos de 7.cpp
@@ -5,14 +5,11 @@ using namespace std;
 // SALIDA: mult1, mult2 ... multn: "n" multiplos de 7
 int main (){
 	
-	int primerosnmultiplos, multiplico;
+	int primerosnmultiplos, multiplico,i;
 	cout << "Indique cuantos primeros multiplos de n quiere ver: ";
 	cin >> primerosnmultiplos;
 	cout << endl;
-	
-	int i = 0;
-	while (i < primerosnmultiplos){
-		i++;
+	for (i=0;i < primerosnmultiplos;i++){
 		multiplico = 7*i;
 		cout << i << ". multiplo de 7 es: " << multiplico << endl;
 	}
